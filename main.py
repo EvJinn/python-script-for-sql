82a738f318d568e061ff255bdef8ddf4c6c31d97
Evgeny Kopylov <zhenya.ko1711@gmail.com>
2021-06-12 22:17:27 +0700

    add comments

--- main.py
+++ main.py
@@ -2,13 +2,15 @@
 
 import pyodbc
 
+# подключение к базе
 connStr = (
     r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
-    r"DBQ=C:\Users\User\Desktop\AUTOSTOP.mdb;"
+    r"DBQ=C:\Users\User\Desktop\AUTOSTOP.mdb;" # путь к файлу с базой. User заменить на имя своего пользователя в винде
 )
 cnxn = pyodbc.connect(connStr)
 cursor = cnxn.cursor()
 
+# запрос
 cursor.execute("""
             SELECT COUNT(CAR.USERID) AS COUNT, USERS.NAME
             FROM CAR INNER JOIN USERS ON CAR.USERID = USERS.ID
@@ -17,13 +19,17 @@ cursor.execute("""
             ORDER BY COUNT(CAR.USERID) DESC;
             """)
 rows = cursor.fetchall()
+
+# открываем файл для записи
 file = open("result.txt", "w+")
 file.write("(COUNT, USERS.NAME)\n")
 
+# выводим результат запроса в консоль и параллельно записываем его в файл
 for row in rows:
     print(row)
     file.write(str(row))
     file.write('\n')
 
+# закрываем файл и подключение к базе
 file.close()
 cnxn.close()
